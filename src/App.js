8dec956edc607921295a14fcbe0a3e0fa426dd12
MrHarshJain <harshjainup17@gmail.com>
2026-07-05 22:03:59 +0530

    Lifting the State Up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// Write your code at relevant places in the code below:
 
-function App() {
+import React,{useState} from "react";
+import Expenses from "./components/Expenses/Expenses";
+import NewExpense from "./components/NewExpense/NewExpense";
+
+const App = () => {
+  const[expenses, setExpenses]= useState( [
+    { id: 1, title: "Insurance", date: new Date(2023, 7, 15), price: 100 },
+    { id: 2, title: "Book", date: new Date(2023, 8, 25), price: 10 },
+    { id: 3, title: "Pen", date: new Date(2023, 2, 10), price: 1 },
+    { id: 4, title: "Laptop", date: new Date(2023, 9, 17), price: 200 },
+  ]
+);
+
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses)=>{
+      return [expense, ...prevExpenses];
+    })
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses expenses={expenses} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/Expenses/expensesFilter.js
@@ -0,0 +1,13 @@
+import React from "react";
+import './ExpensesFilter.css'
+const ExpensesFilter=()=>{
+    <div className="expenses-filter">
+        <select className="expenses-filter">
+            <option value="2023">2023</option>
+            <option value="2024">2024</option>
+            <option value="2025">2025</option>
+        </select>
+    </div>
+};
+
+export default ExpensesFilter;
