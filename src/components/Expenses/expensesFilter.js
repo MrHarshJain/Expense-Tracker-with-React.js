f2caa0878dfb248d60917879c822e5da9dfc6ffb
MrHarshJain <harshjainup17@gmail.com>
2026-07-07 00:44:49 +0530

    Filter expenses by Year functionality added

--- src/components/Expenses/expensesFilter.js
+++ src/components/Expenses/expensesFilter.js
@@ -1,13 +1,23 @@
 import React from "react";
-import './ExpensesFilter.css'
-const ExpensesFilter=()=>{
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+    const  dropdownChangeHandler = (event) =>{
+        props.onChangeFilter(event.target.value)
+    };
+
+    return(
     <div className="expenses-filter">
-        <select className="expenses-filter">
+        <div className="expenses-filter__control">
+        <label htmlFor="year-dropdown">Filter by Year</label>
+        <select value ={props.selected} onChange={dropdownChangeHandler} id="year-dropdown">
             <option value="2023">2023</option>
             <option value="2024">2024</option>
             <option value="2025">2025</option>
         </select>
     </div>
+    </div>
+    );
 };
 
 export default ExpensesFilter;
